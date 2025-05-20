8a2d7c9f02634f1145588f9b7e2b121d4534307e
Crevista <billybickell@hotmail.co.uk>
2025-05-20 08:32:59 +0100

    Update route.js

--- app/api/cancel-subscription/route.js
+++ app/api/cancel-subscription/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import Stripe from 'stripe';
 import { authOptions } from '../auth/[...nextauth]/route';
-import prisma from "../../../lib/prismaClient";
+import { prisma } from "../../../lib/prisma";
 
 // Force dynamic rendering
 export const dynamic = 'force-dynamic';
