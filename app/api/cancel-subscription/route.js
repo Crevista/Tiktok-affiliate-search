f9b2c6ce45a52a91fcdf901ad7b4549baf31b416
Crevista <billybickell@hotmail.co.uk>
2025-05-20 15:11:44 +0100

    Update route.js

--- app/api/cancel-subscription/route.js
+++ app/api/cancel-subscription/route.js
@@ -2,10 +2,10 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]/route';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../../../lib/prisma';
+
 import { stripe } from '../../../lib/stripe'; // Updated import path
 
-const prisma = new PrismaClient();
 
 export async function POST() {
   try {
