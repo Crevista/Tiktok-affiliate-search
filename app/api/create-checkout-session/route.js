373620b99a0466717b6a3cd8a62973c236137eb6
Crevista <billybickell@hotmail.co.uk>
2025-05-24 16:00:36 +0100

    Update route.js

--- app/api/create-checkout-session/route.js
+++ app/api/create-checkout-session/route.js
@@ -1,4 +1,5 @@
 // app/api/create-checkout-session/route.js
+export const dynamic = 'force-dynamic';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]/route';
